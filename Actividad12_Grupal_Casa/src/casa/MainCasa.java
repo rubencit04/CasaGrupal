01eb9317f379ecc7afe21a1157f800bb1590ed49
yolmi <andresgarciasampayo@gmail.com>
2024-03-04 12:28:50 +0100

    Creaciona arrays(1)

--- Actividad12_Grupal_Casa/src/casa/MainCasa.java
+++ Actividad12_Grupal_Casa/src/casa/MainCasa.java
@@ -6,8 +6,11 @@ public class MainCasa {
 
 	public static void main(String[] args) {
 		System.out.println("-----Casa-----");
-		Casa c1 = new Casa(new Direccion("Calle", "Shiny","Kanto" , 25369), 500000, true, 125, new Jardin(30, 2), new ArrayList<Habitacion>());
+		Casa c1 = new Casa(new Direccion("Calle", "Shiny","Kanto" , 25369), 500000, true, 125, new Jardin(30, 2), new ArrayList<Habitacion> ());
 		Casa c2 = new Casa(new Direccion("Avenida", "Ultraente","Alola" , 30029), 204303, false, 300, new Jardin(50, 500), new ArrayList<Habitacion>());
+		
+		new ArrayList<Habitacion> listaHabitacion;
+		c1.setListaHabitaciones(new ArrayList<Habitacion>("4", 52, TipoHabitacion.BAÑO);
 		
 		//Métodos
 		c1.calcularMetrosCuadrados();
