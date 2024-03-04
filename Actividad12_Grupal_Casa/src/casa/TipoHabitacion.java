06e723836c821d40dc361e95d184f28d75081455
MarkDelsius <marcosdelsaz2005@gmail.com>
2024-03-04 11:17:40 +0100

    Arreglado el conflicto de la clase TipoHabitacion

--- Actividad12_Grupal_Casa/src/casa/TipoHabitacion.java
+++ Actividad12_Grupal_Casa/src/casa/TipoHabitacion.java
@@ -1,10 +1,6 @@
 package casa;
 
 public enum TipoHabitacion {
-<<<<<<< HEAD
  SALON, COCINA, BAÑO, SALA_DE_ESTAR;
-=======
-	Salon, Cocina, Baño, Sala_De_Estar
 
->>>>>>> 331257eebbd7e64746db7afe6be741c8cf001dfe
 }
