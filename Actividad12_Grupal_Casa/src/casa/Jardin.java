b65653f5cef9be93eb7790852b73d971cc37c576
MarkDelsius <marcosdelsaz2005@gmail.com>
2024-03-04 10:56:20 +0100

    Modificada la clase Jardin

--- Actividad12_Grupal_Casa/src/casa/Jardin.java
+++ Actividad12_Grupal_Casa/src/casa/Jardin.java
@@ -1,5 +1,24 @@
 package casa;
 
 public class Jardin {
+	
+	private double metrosCuadrados;
+	private double numeroPlantas;
+	public double getMetrosCuadrados() {
+		return metrosCuadrados;
+	}
+	public void setMetrosCuadrados(double metrosCuadrados) {
+		this.metrosCuadrados = metrosCuadrados;
+	}
+	public double getNumeroPlantas() {
+		return numeroPlantas;
+	}
+	public void setNumeroPlantas(double numeroPlantas) {
+		this.numeroPlantas = numeroPlantas;
+	}
+	public String toString() {
+		return "Jardin [metrosCuadrados=" + metrosCuadrados + ", numeroPlantas=" + numeroPlantas + "]";
+	}
+	
 
 }
