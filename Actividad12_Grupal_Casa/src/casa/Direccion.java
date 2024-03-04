a78fce228cb7c071e0308a4668e3f44416ea86a4
rubencit04 <tterubenator@gmail.com>
2024-03-04 11:04:17 +0100

    Clase direccion

---
+++ Actividad12_Grupal_Casa/src/casa/Direccion.java
@@ -0,0 +1,39 @@
+package casa;
+
+public class Direccion {
+	String tipoVia;
+	String nombreVia;
+	String localidad;
+	int codigoPostal;
+	public String getTipoVia() {
+		return tipoVia;
+	}
+	public void setTipoVia(String tipoVia) {
+		this.tipoVia = tipoVia;
+	}
+	public String getNombreVia() {
+		return nombreVia;
+	}
+	public void setNombreVia(String nombreVia) {
+		this.nombreVia = nombreVia;
+	}
+	public String getLocalidad() {
+		return localidad;
+	}
+	public void setLocalidad(String localidad) {
+		this.localidad = localidad;
+	}
+	public int getCodigoPostal() {
+		return codigoPostal;
+	}
+	public void setCodigoPostal(int codigoPostal) {
+		this.codigoPostal = codigoPostal;
+	}
+	@Override
+	public String toString() {
+		return "Direccion [tipoVia=" + tipoVia + ", nombreVia=" + nombreVia + ", localidad=" + localidad
+				+ ", codigoPostal=" + codigoPostal + "]";
+	}
+	
+	
+}
--- Actividad12_Grupal_Casa/src/casa/MainCasa.java
+++ Actividad12_Grupal_Casa/src/casa/MainCasa.java
@@ -3,7 +3,7 @@ package casa;
 public class MainCasa {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 
 	}
 
